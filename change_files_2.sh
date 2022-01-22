99ab7425cdbf86507937db2470b386a5abf569b0
cryptongithub <96590783+cryptongithub@users.noreply.github.com>
2022-01-22 19:29:20 +0300

    Update change_files_2.sh

--- change_files_2.sh
+++ change_files_2.sh
@@ -6,6 +6,7 @@ wget -q -O $HOME/tutorials-simple-aggregation"/schema.graphql" "https://raw.gith
 wget -q -O $HOME/tutorials-simple-aggregation"/project.yaml" "https://raw.githubusercontent.com/cryptongithub/Subquery-4-prepared_file_change/main/project.yaml_2" 
 wget -q -O $HOME/tutorials-simple-aggregation/src/mappings"/mappingHandlers.ts" "https://raw.githubusercontent.com/cryptongithub/Subquery-4-prepared_file_change/main/mappingHandlers.ts_2" 
 cd $HOME/tutorials-simple-aggregation 
-yarn codegen && yarn build 
+yarn codegen 
+yarn build 
 docker-compose pull 
 docker-compose up
