7aa3b6ba1cf94bc17cc27b7b65c4227cb58be4cc
cryptongithub <96590783+cryptongithub@users.noreply.github.com>
2022-01-22 18:31:46 +0300

    Update change_files_2.sh

--- change_files_2.sh
+++ change_files_2.sh
@@ -1,11 +1,2 @@
-sudo rm -r $HOME/tutorials-simple-aggregation/schema.graphql
-sudo rm -r $HOME/tutorials-simple-aggregation/project.yaml 
-sudo rm -r $HOME/tutorials-simple-aggregation/src/mappings/mappingHandlers.ts
-wget -q -O $HOME/tutorials-simple-aggregation"/schema.graphql" "https://raw.githubusercontent.com/cryptongithub/Subquery-4-prepared_file_change/main/schema.graphql_2" 
-wget -q -O $HOME/tutorials-simple-aggregation"/project.yaml" "https://raw.githubusercontent.com/cryptongithub/Subquery-4-prepared_file_change/main/project.yaml_2" 
-wget -q -O $HOME/tutorials-simple-aggregation/src/mappings"/mappingHandlers.ts" "https://raw.githubusercontent.com/cryptongithub/Subquery-4-prepared_file_change/main/mappingHandlers.ts_2" 
-cd $HOME/tutorials-simple-aggregation
-yarn codegen 
-yarn build 
-docker-compose pull 
-docker-compose up
+sudo rm -r $HOME/tutorials-simple-aggregation/schema.graphql && sudo rm -r $HOME/tutorials-simple-aggregation/project.yaml && sudo rm -r $HOME/tutorials-simple-aggregation/src/mappings/mappingHandlers.ts && wget -q -O $HOME/tutorials-simple-aggregation"/schema.graphql" "https://raw.githubusercontent.com/cryptongithub/Subquery-4-prepared_file_change/main/schema.graphql_2" && wget -q -O $HOME/tutorials-simple-aggregation"/project.yaml" "https://raw.githubusercontent.com/cryptongithub/Subquery-4-prepared_file_change/main/project.yaml_2" && wget -q -O $HOME/tutorials-simple-aggregation/src/mappings"/mappingHandlers.ts" "https://raw.githubusercontent.com/cryptongithub/Subquery-4-prepared_file_change/main/mappingHandlers.ts_2" && cd $HOME/tutorials-simple-aggregation
+cd $HOME/tutorials-simple-aggregation && yarn codegen && yarn build && docker-compose pull && docker-compose up
