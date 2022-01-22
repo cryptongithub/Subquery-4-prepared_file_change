7c60e07bdfcdd1f382155f535027991207e1bc45
cryptongithub <96590783+cryptongithub@users.noreply.github.com>
2022-01-22 20:40:39 +0300

    Update initial_change.sh

--- initial_change.sh
+++ initial_change.sh
@@ -1,4 +1,5 @@
-curl -s https://raw.githubusercontent.com/cryptongithub/init/main/logo.sh | bash
+curl -s https://raw.githubusercontent.com/cryptongithub/init/main/logo.sh | bash && sleep 1
+echo '\n\e[42mSubQuery Module 4 Exercise 1\e[0m\n' && sleep 2
 cd $HOME
 git clone https://github.com/cryptongithub/tutorials-simple-aggregation.git 
 cd tutorials-simple-aggregation 
