9c6b57488bd48e99dcb0462214bb0614f61c9bd9
cryptongithub <96590783+cryptongithub@users.noreply.github.com>
2022-01-22 19:57:53 +0300

    Update initial_change.sh

--- initial_change.sh
+++ initial_change.sh
@@ -1,3 +1,4 @@
+curl -s https://raw.githubusercontent.com/cryptongithub/init/main/logo.sh | bash
 cd $HOME
 git clone https://github.com/cryptongithub/tutorials-simple-aggregation.git 
 cd tutorials-simple-aggregation 
